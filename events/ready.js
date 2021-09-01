e984789df046e67f3f3532230c06ffe7885f8f62
Glitch (bronzed-diagdsfdsfdsfsdfsdfnostic-wakeboard) <none>
2021-09-01 18:43:00 +0000

    🐁⛹ Checkpoint ./events/ready.js:27484636/51

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Ulash Studio》 Gerekli kurulum tamamlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Ulash Studio》 ${client.user.tag} olarak giriş sağlandı...`);
-  client.user.setActivity(`oyna.ytsworld.ml koruması`, {type: 4})
+  client.user.setActivity(`ytsisland.batihost.net in koruması`, {type: 4})
 
 
 };
